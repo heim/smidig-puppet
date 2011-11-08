c37a5e87c13676b2ba4edfa1de0405598a46e113
Andreas Heim <andreas@heim.no>
2011-11-08 20:53:31 +0100

    added plugins for jenkins

--- manifests/ci.pp
+++ manifests/ci.pp
@@ -1,3 +1,6 @@
+include "classes/**"
+include "nodeclasses/**"
+
 Exec { path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games", }
 
 include ci
--- manifests/nodeclasses/ci.pp
+++ manifests/nodeclasses/ci.pp
@@ -2,6 +2,7 @@ class ci inherits base {
   $mysql_password = "w00t"
   
   include jenkins
+  include jenkins::plugins
   include nexus
   include mysql::server
   include users::admins
