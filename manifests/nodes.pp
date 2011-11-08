98e8f66fd08f2ed3156639fd291fb10ed5635a2f
Andreas Heim <andreas@heim.no>
2011-11-08 15:07:02 +0100

    typo

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,21 +1,13 @@
 node basenode {
   Exec { path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games", }
-  
-  include users
-  
-  package { "git-core":
-    ensure => latest,
-  }
 }
 
 node "node2.morisbak.net" inherits basenode {
   include web
-  include deploy_user
 }
 
 node "node3.morisbak.net" inherits basenode {
   include web
-  include deploy_user
 }
 
 node "node1.morisbak.net" inherits basenode {
