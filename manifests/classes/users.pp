5d1a49ee81aaab1f64a3c1bd565d91e64a1f4de1
Andreas Heim <andreas@heim.no>
2011-11-08 15:11:26 +0100

    welcome to typoland: with a vengeance

--- manifests/classes/users.pp
+++ manifests/classes/users.pp
@@ -1,6 +1,6 @@
 class users {}
 
-class users::admin inherits users { 
+class users::admins inherits users { 
   ssh_authorized_key { 'stein@mac-stimori.local':
     ensure => present,
     key => "AAAAB3NzaC1yc2EAAAABIwAAAQEAo114ixCPpMS66KVIsj+wEKyjpNSIh48abP8bAK73JHFNVqyJn1kz015oyoc0NZJn7pd318evAlYF82Vu25cpJK7rFL+2QvI/asgrj0XmwV3DUuqWlieV6DR6XdWHJYUPvs+mPftX23ctmbIO7JfUPZ+pD5ixCdcvAQYFTu6ofwSP51sR7JXv3fxoHhfS03u65893IIjm8ct/tHwBBM76q1ln4TIMIUWFgFFDjoUwWbve8nXBpF0iwVJk93Owde19tDfaLj0cbCVIjkTHbTi9z9N68vBZbGzZLDKDZB6C72/4WOD7JhQ51jBkrufNCQTIagGC5OF7/EMdJRgQ9rNUVw==",
