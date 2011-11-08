bbd30316cd9d35f6bcdb31c720ff3fb0e9040884
Andreas Heim <andreas@heim.no>
2011-11-08 15:12:27 +0100

    DRYing up

--- manifests/nodeclasses/web.pp
+++ manifests/nodeclasses/web.pp
@@ -6,18 +6,4 @@ class web inherits base {
     ensure => latest,
   }
 
-  group { 'deploy': ensure => present, gid => 1337 } 
-  file { '/server':
-    ensure => directory,
-  } 
-  user { 'bekkopen': 
-    ensure => present,
-    password => 'smidig2011',
-    uid => 1337,
-    gid => 1337,
-    shell   => "/bin/bash",
-    home    => "/server/bekkopen",
-    managehome => true,
-    require => File["/server"],
-  }
 }
